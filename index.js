ed629c3c32c2a59129821ec44310f882b099937f
VovikZip <71176812+VovikZip@users.noreply.github.com>
2022-06-28 10:13:26 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { Telegraf } = require('telegraf')
 
-let range = 800
+let range = 300
 const bot = new Telegraf("5446372357:AAF2Yg5oFQ0utgK1iZ-5NvOUAFxSre3tTxs")
 bot.start((ctx) => ctx.reply('1000-7?'))
 bot.hears(`hola`, (ctx) => {
